e8bd79e8b6a5146d1ef52d1ef69b1b58f2660c8d
José Carlos Siqueira <jose.a.siqueira@capgemini.com>
2020-02-04 10:40:55 -0300

    Criando um interceptor para tratamento de erros

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -24,9 +24,8 @@ export class CategoriasPage {
     this.categoriaService.findAll()
       .subscribe(response => {
         this.items = response;
-      }, error => {
-        console.log(error);
-      });
+      }, 
+      error => {});
   }
 
 }
